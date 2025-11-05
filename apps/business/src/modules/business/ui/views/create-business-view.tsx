5d95c25f7ff682d96f34e3c8d0062e5dbc970a38
Joss Bleach <joss.bleach@outlook.com>
2025-11-05 12:18:35 +0000

    Refactor BusinessNavbar and CreateBusinessForm components for improved navigation and UI consistency
    - Wrapped the logo in a Link component for better navigation to the business profile.
    - Enhanced the Stepper component in CreateBusinessForm to display completion status with check icons.
    - Adjusted layout and styling for better responsiveness and user experience.

--- apps/business/src/modules/business/ui/views/create-business-view.tsx
+++ apps/business/src/modules/business/ui/views/create-business-view.tsx
@@ -2,19 +2,15 @@ import { CreateBusinessForm } from "../components/create-business-form";
 
 export const CreateBusinessView = () => {
   return (
-    <main className="w-screen py-6">
-      <div className="mx-auto w-[87.5%] md:w-[692px] lg:w-[980px]">
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold text-foreground">
-            Create your <br />
-            business profile
-          </h1>
-          <p className="font-muted-foreground mt-[14px]">
-            Add your business details below to start reaching local customers.
-          </p>
-          <CreateBusinessForm />
-        </div>
-      </div>
-    </main>
+    <div className="flex flex-col">
+      <h1 className="text-2xl font-semibold text-foreground">
+        Create your <br />
+        business profile
+      </h1>
+      <p className="font-muted-foreground mt-[14px]">
+        Add your business details below to start reaching local customers.
+      </p>
+      <CreateBusinessForm />
+    </div>
   );
 };
